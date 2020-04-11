06d264f63c043c88da21198652bd66bf6d8a552a
prudx <dnl.maguire@gmail.com>
2020-04-11 12:55:46 +0100

    MultiCalc initial UI for ratioCalc implementation

---
+++ RatioCalculator/RatioCalculator/Models/Ratio.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RatioCalculator.Models
+{
+    class Ratio
+    {
+        public double Left;
+        public double Right;
+    }
+}
